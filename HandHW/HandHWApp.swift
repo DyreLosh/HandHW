d5228539ad5f275ec466adb3ffa0f4d698cc2185
DyreLosh <dyrelosh@gmail.com>
2025-07-22 00:23:32 -0700

    first commit

---
+++ HandHW/HandHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandHWApp.swift
+//  HandHW
+//
+//  Created by dinar on 22.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct HandHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
